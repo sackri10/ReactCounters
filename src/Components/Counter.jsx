4e4980845fb6fd67550c0991682be97f691093d8
Datta Sai Krishna <sackri10@gmail.com>
2019-12-03 10:23:43 +0530

    format error

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -24,7 +24,8 @@ class Counter extends Component {
           </button>
           <button
             onClick={() => this.props.onDecrement(this.props.counter)}
-            className=" btn btn-secondary btn-sm m-2" disabled={this.props.counter.value ===0}
+            className=" btn btn-secondary btn-sm m-2"
+            disabled={this.props.counter.value === 0}
           >
             -
           </button>
@@ -32,7 +33,6 @@ class Counter extends Component {
             className=" btn btn-primary btn-sm m-2"
             onClick={() => this.props.onDelete(this.props.counter.id)}
           >
-            {" "}
             Delete
           </button>
         </div>
